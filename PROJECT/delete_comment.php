55a3823f1e6b458493498515cfe1e6aff24dbd1a
Szegedi Tamás <102833465+Shinjiterubx@users.noreply.github.com>
2022-10-12 21:21:41 +0200

    back-end: commit hiba javítás

--- PROJECT/delete_comment.php
+++ PROJECT/delete_comment.php
@@ -1,21 +1,14 @@
 <?php
 include 'connect.php';
 
-$select_id=$_POST['modifyBtn'];
-$modify_ID=$_POST['pid'];
+$select_id=$_POST['deleteBtn'];
 
-$title = $_POST['inputTitle'];
-$comment = $_POST['inputText'];
+var_dump($_POST['deleteBtn']);
 
-if ($modify_ID == 0){ 
-  $modify_ID = 1;
-  header("Location: index.php");
-} 
-else{
-  $update=mysqli_query($conn,"UPDATE posts SET 
-  title='$title', comment='$comment' WHERE modify='$select_id'");
-  $modify_ID = 0;
-  echo "<script>alert('Sikeres Frissítés!');</script>";
-  header("Location: index.php");
-}
+$delete=mysqli_query($conn,"DELETE FROM posts WHERE pid='".$select_id."'");
+
+if($delete){
+    echo "<script>alert('Sikeres Eltávolította');</script>";
+    header("Location: index.php");
+  }
 
